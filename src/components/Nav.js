737328b61bf956c1f0cb11dc791133c845cf5ee8
RkyMtnLSX <132773077+RkyMtnLSX@users.noreply.github.com>
2026-06-11 09:30:39 -0600

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -150,6 +150,11 @@ export default function Nav({ isAdmin, onAdminClick }) {
             Track Correlations
           </Link>
 
+          {/* Qualifying Center */}
+          <Link to="/qualifying" style={linkStyle(location.pathname === '/qualifying')}>
+            Qualifying
+          </Link>
+
         </div>
 
         {/* Right side */}
